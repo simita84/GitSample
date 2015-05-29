5e055c823160afb4d8f3964543a31e68fb6c60ab
simi <simi.tresa.antony@gmail.com>
2015-05-29 14:47:28 -0700

    Initial Commit

---
+++ GitTestSamples/src/test/sample/calc/Calculator.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package test.sample.calc;
+
+/**
+ * @author Joanna
+ *
+ */
+public class Calculator {
+
+	/**
+	 * 
+	 */
+	public Calculator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
